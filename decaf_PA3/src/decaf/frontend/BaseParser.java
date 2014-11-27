ae6c05ebdc7b392ebfdb46147d93c940b3829a8b
柯均洁 <kjj3300@163.com>
2014-11-27 15:13:49 +0800

    change Parser.java to PA2's. Don't know its effects

--- decaf_PA3/src/decaf/frontend/BaseParser.java
+++ decaf_PA3/src/decaf/frontend/BaseParser.java
@@ -54,17 +54,17 @@ public abstract class BaseParser {
 	 */
 	public static String opStr(int opCode) {
 		switch (opCode) {
-		case Parser.AND:
+		case Tree.AND:
 			return "&&";
-		case Parser.EQUAL:
+		case Tree.EQ:
 			return "==";
-		case Parser.GREATER_EQUAL:
+		case Tree.GE:
 			return ">=";
-		case Parser.LESS_EQUAL:
+		case Tree.LE:
 			return "<=";
-		case Parser.NOT_EQUAL:
+		case Tree.NE:
 			return "!=";
-		case Parser.OR:
+		case Tree.OR:
 			return "||";
 		case Tree.PLUS:
 			return "+";
